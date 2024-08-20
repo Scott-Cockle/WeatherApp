4471cc83cb087327fd407f4e488ac020ca23d667
Scott-Cockle <scott.b.cockle@gmail.com>
2024-08-20 01:50:21 +0100

    added 'try again' feature after failed user input

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ weather_data = {city.lower(): data for city, data in weather_data.items()}
 name_confirm = input("Hello! What is your name?: ")
 print(f"\nWelcome {name_confirm}!")
 
-location = input("\nWhich city would you like to know the weather for?: ")
-
-location = location.lower()
-
 def get_weather(city):
     if city in weather_data:
         data = weather_data[city]
@@ -18,8 +14,14 @@ def get_weather(city):
                 f"Wind MPH: {data['wind']} \n"
                 f"Humidity: {data['humidity']} \n"
                 f"\nThank you for using our weather app! We hope to see you again soon :)")
-    else:
-        return (f"\nSorry, I'm unable to gather information about the weather in {city} at this time.")
+        return None
 
-weather_info = get_weather(location)
-print(weather_info)
+while True:
+    location = input("\nWhich city would you like to know the weather for?: ")
+    weather_info = get_weather(location)
+    
+    if weather_info:
+        print(weather_info)
+        break
+    else:
+        print(f"\nSorry, I'm unable to gather information about the weather in {location} at this time. Please try again.")
